f379b01ca60a557a8278b72699aa10a8fb398c72
Divyanshu Sahu <dsahu1997@gmail.com>
2019-05-21 13:28:02 +0530

    minor changes

--- script.js
+++ script.js
@@ -2,5 +2,15 @@ $(document).ready(function() {
     $('#sidebarCollapse').on('click', function() {
         $('#sidebar').toggleClass('active');
         $(this).toggleClass('active');
+
     });
+
+    $("#btn_round_canopy").on('click', function() {
+        $("#round_canopy_modal").toggleClass("pop_up_toggle");
+        //$("#input_round_canopy").toggleClass("pop_up_toggle");
+    });
+
+    /*$(".input_close_button").on("click", function() {
+        $(".custom_modal").toggleClass("pop_up_toggle");
+    })*/
 });
