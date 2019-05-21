9a863f1168098e36cc1343ef07287e300d9d47d5
Divyanshu Sahu <dsahu1997@gmail.com>
2019-05-21 13:59:19 +0530

    input parameter modal display functionality done

--- script.js
+++ script.js
@@ -7,10 +7,41 @@ $(document).ready(function() {
 
     $("#btn_round_canopy").on('click', function() {
         $("#round_canopy_modal").toggleClass("pop_up_toggle");
-        //$("#input_round_canopy").toggleClass("pop_up_toggle");
     });
 
-    /*$(".input_close_button").on("click", function() {
-        $(".custom_modal").toggleClass("pop_up_toggle");
-    })*/
+    $("#btn_planform").on('click', function() {
+        $("#planform_modal").toggleClass("pop_up_toggle");
+    });
+
+    $("#btn_airfoil").on('click', function() {
+        $("#airfoil_modal").toggleClass("pop_up_toggle");
+    });
+
+    $("#btn_volute").on('click', function() {
+        $("#volute_modal").toggleClass("pop_up_toggle");
+    });
+
+    $("#btn_anchor_location").on('click', function() {
+        $("#anchor_location_modal").toggleClass("pop_up_toggle");
+    });
+
+    $("#btn_flat_panels").on('click', function() {
+        $("#flat_panels_modal").toggleClass("pop_up_toggle");
+    });
+
+    $("#btn_rigging_lines_arrangement").on('click', function() {
+        $("#rigging_lines_arrangement_modal").toggleClass("pop_up_toggle");
+    });
+
+    $("#btn_brake_lines").on('click', function() {
+        $("#brake_lines_modal").toggleClass("pop_up_toggle");
+    });
+
+    $("#btn_advance_input").on('click', function() {
+        $("#advance_input_modal").toggleClass("pop_up_toggle");
+    });
+
+    $(".input_close_button").on("click", function() {
+        $(".custom_modal").addClass("pop_up_toggle");
+    })
 });
