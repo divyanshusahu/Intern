e562b8bd4e015c206bb7cb36b6d5c650f63bcfe8
Divyanshu Sahu <dsahu1997@gmail.com>
2019-05-23 21:27:05 +0530

    vue version added

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  
+  <div id="app">
+
+    <div class="wrapper">
+      <sidebar />
+    </div>
+
+  </div>
+
+</template>
+
+<script>
+import sidebar from './components/sidebar.vue'
+
+export default {
+  name: 'app',
+  components: {
+    sidebar
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /*text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;*/
+}
+
+@import 'assets/css/wrapper.css';
+@import 'assets/css/sidebar.css';
+
+</style>
---
+++ src/components/sidebar.vue
@@ -0,0 +1,32 @@
+<template>
+  
+	<nav id="sidebar">
+    
+		<div class="sidebar-header">
+      <h3>Software Name</h3>
+    </div>
+
+    <ul class="list-unstyled components">
+      <li>
+        <a href="#file" data-toggle="collapse" aria-expended="false" class="dropdown-toggle">File</a>
+        <ul class="collapse list-unstyled" id="file">
+          <li>
+            <a href="#">New</a>
+          </li>
+          <li>
+            <a href="#">Open</a>
+          </li>
+          <li>
+            <a href="#">Import VTK</a>
+          </li>
+          <li class="divider"></li>
+          <li>
+            <a href="#">Exit</a>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  
+	</nav>
+
+</template>
